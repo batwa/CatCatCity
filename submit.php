13794da557545a83eaa7791fa2a38b908ea8dee5
wch94 <hess.372@osu.edu>
2015-11-14 15:52:12 -0500

    changes

--- submit.php
+++ submit.php
@@ -1,20 +1,11 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>"You are doing the Survey!"</title>
-	<link rel="stylesheet" href="css/normalize.css">
-	<link href='http://fonts.googleapis.com/css?family=Merriweather' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" href="css/main.css">
-</head>
-<body>
-	<header>
-		<?php
-			echo "Answer 1 :".$_POST['q1'];
-			echo "Answer 1 :".$_POST['q2'];
-			echo "Answer 1 :".$_POST['q3'];
-			echo "Answer 1 :".$_POST['q4'];
-		?>
-	</header>
-</body>
-</html>
+
+<?php
+	echo "Answer 1 :".$_GET['q1'];
+	echo "Answer 2 :".$_GET['q2'];
+	echo "Answer 3 :".$_GET['q3'];
+	echo "Answer 4 :".$_GET['q4'];
+	echo "Answer 1 :".$_POST['q1'];
+	echo "Answer 2 :".$_POST['q2'];
+	echo "Answer 3 :".$_POST['q3'];
+	echo "Answer 4 :".$_POST['q4'];
+?>
