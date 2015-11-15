4ae25e8ad8841c17b12cfcac20f485ea0b9e260c
wch94 <hess.372@osu.edu>
2015-11-14 22:09:10 -0500

    sdfdsfs

--- submit.php
+++ submit.php
@@ -8,16 +8,8 @@
 	<link rel="stylesheet" href="css/survey.css">
 </head>
 <body>
-	<p>Herllo<p>
-	<?php echo "Answer 1 :".$_POST['q1']; 
-	echo "Answer 2 :".$_POST['q2']; 
-	echo "Answer 3 :".$_POST['q3']; 
-	echo "Answer 4 :".$_POST['q4']; 
-	echo "Answer 5 :".$_POST['q5']; 
-	echo "Answer 6 :".$_POST['q6']; 
-	echo "Answer 7 :".$_POST['q7']; 
-	echo "Answer 8 :".$_POST['q8']; 
-	echo "Answer 9 :".$_GET['q9']; 
-	echo "Answer 10 :".$_GET['q10']; ?>
+	<header>
+	
+	</header>
 </body>
 </html>
