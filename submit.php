d83ad95f4b464c24d96b7d7e5c8cd866aea0e26e
wch94 <hess.372@osu.edu>
2015-11-14 19:10:42 -0500

    dksijhdisd

---
+++ submit.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>"You are doing the Survey!"</title>
+	<link rel="stylesheet" href="css/normalize.css">
+	<link href='http://fonts.googleapis.com/css?family=Merriweather' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/survey.css">
+</head>
+<body>
+	<header>
+		<p><?php echo 'Answer 1 :'.$_GET["q1"]; ?><p>
+		<p><?php echo "Answer 1 :".$_GET['q2']; ?><p>
+		<p><?php echo "Answer 1 :".$_GET['q3']; ?><p>
+		<p><?php echo "Answer 1 :".$_GET['q4']; ?><p>
+	</header>
+</body>
+</html>
